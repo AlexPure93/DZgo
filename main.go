6eb0fd6d3172e8683388af9e7c5b557673a21c51
AlexPure <xxxpridexxx1@yandex.ru>
2026-03-27 09:48:40 +0300

    dz2

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	const USDinUER float64 = 0.86
+	const USDinEUR float64 = 0.86
 	const USDinRUB float64 = 82.13
-	EURinRUB := 1 * (USDinUER / USDinRUB)
+	EURinRUB := USDinRUB / USDinEUR
 	fmt.Println(EURinRUB)
 }
