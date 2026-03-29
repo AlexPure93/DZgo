5e8db2585ab87c6c53b853d1287ac40419e998a3
AlexPure <xxxpridexxx1@yandex.ru>
2026-03-29 12:05:12 +0300

    dz2

--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	const USDinEUR float64 = 0.86
-	const USDinRUB float64 = 82.13
-	EURinRUB := USDinRUB / USDinEUR
-	fmt.Println(EURinRUB)
+
 }
+
+func getInput() int {
+	num := 0
+	fmt.Println("Введите сумму для конвертации")
+	fmt.Scan(&num)
+	return num
+}
+
+func conversion(a int, b float64, c float64) {}
